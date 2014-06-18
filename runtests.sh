65455b5bd0ae9c830f7bbbf93120a70fd3deb09f
Abhijeet <void.aby@gmail.com>
2014-06-19 00:58:53 +0530

    added new badge in readme

---
+++ runtests.sh
@@ -0,0 +1,14 @@
+python setup.py develop
+echo "======================================================================"
+echo
+if [ "$1" == "remote" ] ; then
+    REMOTE=true python -m unittest discover tests -v
+else 
+    python -m unittest discover tests -v
+fi
+echo "======================================================================"
+echo
+coverage report -m
+echo "======================================================================"
+echo
+yes | pip uninstall TheZine
